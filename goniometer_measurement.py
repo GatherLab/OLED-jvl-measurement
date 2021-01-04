2451843963cf918eee7bfda0f4adeb01224e9a51
BuJ1 <JulianButscher@gmx.de>
2021-01-04 17:18:02 +0100

    [goniometer] started to implement PL code

--- goniometer_measurement.py
+++ goniometer_measurement.py
@@ -249,6 +249,231 @@ class GoniometerMeasurement(QtCore.QThread):
         self.save_spectrum_data()
         self.hide_progress_bar.emit()
 
+        # --------------------------------------------------------------------- #
+        # -------------------------- PL from here ----------------------------- #
+        # --------------------------------------------------------------------- #
+        # "INITIALIZING SETTINGS"
+        # defaults = {}
+        # settings = {}
+        # parameters = []
+        #
+        # "SETTING DEFAULT PARAMETERS"
+        # defaults[0] = "test"  # sample
+        # defaults[1] = 314  # offset_angle
+        # defaults[2] = 1  # step_angle
+        # defaults[3] = 500000  # integrationtime
+        # defaults[4] = 20.0  # homing_time
+        # defaults[5] = 0.2  # moving_time
+        # defaults[6] = 0.2  # pulse_duration
+        # defaults[7] = "HL"  # ang_range
+        #
+        # "GETTING PARAMETERS FROM GUI, IF BLANK SETTING AS DEFAULT"
+        # for x in range(0, 8, 1):
+        # if not param[x]:
+        # parameters.append(defaults[x])
+        # else:
+        # settings[x] = param[x]
+        # parameters.append(settings[x])
+        # if parameters[7] == "F":
+        # self.min_angle = float(parameters[1]) - 90
+        # self.max_angle = float(parameters[1]) + 90
+        # elif parameters[7] == "HL":
+        # self.min_angle = float(parameters[1])
+        # self.max_angle = float(parameters[1]) + 90
+        # elif parameters[7] == "HR":
+        # self.min_angle = float(parameters[1]) - 90
+        # self.max_angle = float(parameters[1])
+        # else:
+        # self.queue.put("Invalid input.")
+        #
+        # "SETTING PARAMTERS"
+        # self.sample = parameters[0]
+        # self.offset_angle = float(parameters[1])
+        # self.step_angle = float(parameters[2])
+        # self.integrationtime = float(parameters[3])
+        # self.homing_time = float(parameters[4])
+        # self.moving_time = float(parameters[5])
+        # self.pulse_duration = float(parameters[6])
+        # self.ang_range = parameters[7]
+
+        # "SETTING DIRECTORY DETAILS"
+        # # Getting date-time
+        # now = dt.datetime.now()
+        # datetime = str(
+        #     now.strftime("%Y-%m-%d %H:%M")
+        #     .replace(" ", "")
+        #     .replace(":", "")
+        #     .replace("-", "")
+        # )
+        # self.queue.put(
+        #     "Measurement code : "
+        #     + self.sample
+        #     + datetime
+        #     + "  (OLED device code followed by the datetime of measurement)."
+        # )
+        # # Set directories for recorded data.
+        # directory = os.path.abspath(
+        #     os.path.join(
+        #         os.path.dirname(__file__), "data", self.sample, datetime, "raw"
+        #     )
+        # )  # Folder to separate raw and processed data
+        # if os.path.isdir(directory):
+        #     pass
+        # else:
+        #     os.makedirs(directory)
+
+        """
+        "SETTING FILE DETAILS"
+        # Filename Parameters
+        mayafilepath = os.path.join(directory, "spectrumdata")
+        if os.path.isdir(mayafilepath):
+            pass
+        else:
+            os.makedirs(mayafilepath)
+
+        mayafilename = "Background.txt"  # Set filename for background reading
+        # Header Parameters
+        line01 = "Measurement code : " + self.sample + datetime
+        line02 = (
+            'Measurement programme :	"GatherLab Goniometer Measurement System".py'
+        )
+        linex = "Credits :	GatherLab, University of St Andrews, 2020"
+        linexx = "Integration Time:  " + str(self.integrationtime) + "micro s"
+        line03 = "Pulse duration :		" + str(self.pulse_duration) + " s"
+        line04 = "Step time between voltages :		" + str(self.moving_time) + " s"
+        line05 = "Offset angle:     " + str(self.offset_angle)
+        line06 = (
+            "Min Angle:  "
+            + str(0)
+            + "Max Angle:  "
+            + str(90)
+            + "Step Angle:  "
+            + str(self.step_angle)
+        )
+        line07 = "### Measurement data ###"
+        # line08 = 'OLEDVoltage	OLEDCurrent Photodiode Voltage'
+        # line09 = 'V	              mA               V'
+        # line10 = 'Angle    OLEDVoltage   	OLEDCurrent'
+        # line11 = 'Degrees 	  V       	 A'
+        line12 = "Wavelength   Intensity"
+        line13 = "nm             -"
+
+        # header_lines = [line01, line02, linex, linexx, line03, line04, line05, line06, line07, line08, line09] # PD Voltages
+        # header_lines2 = [line01, line02, linex, linexx, line03, line04, line05, line06, line07, line10, line11] # OLED Voltages
+        header_lines3 = [
+            line01,
+            line02,
+            linex,
+            linexx,
+            line03,
+            line04,
+            line05,
+            line06,
+            line07,
+            line12,
+            line13,
+        ]  # Spectrum Data
+
+        "MOVING TO INITIAL POSITION"
+        DEVICES.PLmotor.move_to(self.min_angle)
+        time.sleep(self.homing_time)
+
+        self.queue.put("\nOceanOptics : " + str(DEVICES.MAYA_devices[0]))
+        DEVICES.spec.integration_time_micros(self.integrationtime)
+
+        "#####################################################################"
+        "####TAKING MEASUREMENTS FROM THE OCEANOPTICS MAYALSL SPECTROMETER####"
+        "#####################################################################"
+
+        self.queue.put("\n\nSPECTROMETER READINGS")
+
+        "IMPLEMENTATION"
+        # Generate empty lists for data collection
+        ang = []  # Angles
+        wvl = []  # Wavelengths
+        inte = []  # Intensities
+        spect = []  # Spectrums
+
+        # Take calibration readings
+        spectrum = (
+            DEVICES.spec.spectrum()
+        )  # this gives a pre-stacked array of wavelengths and intensities
+        np.savetxt(
+            os.path.join(directory, mayafilename),
+            spectrum.T,
+            fmt="%.4f %.0f",
+            delimiter="\t",
+            header="\n".join(header_lines3),
+            comments="",
+        )
+        processing_time = 0.5  # Initial processing time in seconds
+
+        for angle in np.arange(self.min_angle, self.max_angle + 1, self.step_angle):
+            DEVICES.PLmotor.move_to(angle)
+            time.sleep(self.moving_time)
+
+            time.sleep(self.pulse_duration)
+            time.sleep(self.pulse_duration - processing_time)
+            start_process = time.clock()
+
+            wavelength = DEVICES.spec.wavelengths()  # creates a list of wavelengths
+            intensity = DEVICES.spec.intensities()  # creates a list of intensities
+            spectrum = (
+                DEVICES.spec.spectrum()
+            )  # this gives a pre-stacked array of wavelengths and intensities
+            wvl.append(
+                wavelength
+            )  # adding to a master list (may not be necessary with txt file outputs)
+            inte.append(
+                intensity
+            )  # adding to a master list (may not be necessary with txt file outputs)
+            spect.append(
+                spectrum
+            )  # adding to a master list (may not be necessary with txt file outputs)
+
+            "DISPLAYING SPECTRUM AS A PLOT"
+            self.queue.put([wavelength, intensity])
+
+            "SPECTRUM OUTPUT FILE"
+            # Angle is written as 0 -> 180 rather than -90 -> 90
+            if self.ang_range == "F":
+                mayafilename = (
+                    "Angle" + str(angle + 90 - self.offset_angle).zfill(3) + ".txt"
+                )  # changing mayalsl filename for actual readings
+            elif self.ang_range == "HL":
+                mayafilename = (
+                    "Angle" + str(angle - self.offset_angle).zfill(3) + ".txt"
+                )  # changing mayalsl filename for actual readings
+            elif self.ang_range == "HR":
+                mayafilename = (
+                    "Angle" + str(self.offset_angle - angle).zfill(3) + ".txt"
+                )  # changing mayalsl filename for actual readings
+            mayafilename = os.path.abspath(os.path.join(mayafilepath, mayafilename))
+            np.savetxt(
+                mayafilename,
+                spectrum.T,
+                fmt="%.3f %.0f",
+                delimiter="\t",
+                header="\n".join(header_lines3),
+                comments="",
+            )
+            end_process = time.clock()
+            processing_time = end_process - start_process
+            self.queue.put("\nProcessing time :  " + str(processing_time))
+
+            if self.ang_range == "F":
+                self.queue.put("\nAngle : " + str(angle + 90 - self.offset_angle))
+                ang.append(angle + 90 - self.offset_angle)
+            elif self.ang_range == "HL":
+                self.queue.put("\nAngle : " + str(angle - self.offset_angle))
+                ang.append(angle - self.offset_angle)
+            elif self.ang_range == "HR":
+                self.queue.put("\nAngle : " + str(self.offset_angle - angle))
+                ang.append(self.offset_angle - angle)
+
+        self.queue.put("\n\nMEASUREMENT COMPLETE")
+        """
+
     def save_iv_data(self):
         """
         Function to save the iv data that contains the data of current and
